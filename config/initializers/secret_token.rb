9e7736d8ab98ff46a466dc7eb5275f2c69bd2738
Sheng-Loong SU <sushengloong@gmail.com>
2011-08-11 22:29:02 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheerlist::Application.config.secret_token = '7deaddb03fff741a87e7dd19e47990605ceea7f64bdda587d0e8becba03e8e939fc8294a0f72a16ade4bd76b16aa65c480d3c10104081c8b18ae1303202a57ae'
