62ddc1e89bb91a3c6488ab8c445afdccc58cf9bb
Sheng-Loong SU <sushengloong@gmail.com>
2011-08-13 00:07:52 +0800

    added fancybox and improved user authentication

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,22 @@
+jQuery(document).ready(function() {
+	
+	jQuery("a#login_link").fancybox({
+		'width' : 500,
+		'height' : 360,
+		'padding': 25,
+		'autoDimensions': false,
+		'overlayOpacity' : 0.4,
+		'overlayColor' : 'black',
+		'overlayShow' : true,
+		'scrolling' : 'no',
+		'titleShow' : true,
+		'onClosed' : function() {
+			//$("#login_error").hide();
+		}
+	});
+
+});
+
 function about() {
 	info = document.getElementById('about-content');
 	if(window.XMLHttpRequest) {
