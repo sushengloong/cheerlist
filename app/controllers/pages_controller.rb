9fe44fb8d54b9ce4c5e36b68fd7d75d1ea39c0f3
Sheng-Loong SU <sushengloong@gmail.com>
2011-09-04 23:26:28 +0800

    added map to index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def index
-    
+    ip = Rails.env.production? ? request.ip : "60.48.210.169"
+    @location = Location.new(:ip_address => ip)
+    @location.geocode
+    @location.reverse_geocode
+    @json = @location.to_gmaps4rails
   end
   
   def about
