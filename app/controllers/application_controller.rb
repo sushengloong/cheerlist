d4f546e4c13b42ca295bb3cd8c4442579a1dc5c0
Sheng-Loong Su <sushengloong@gmail.com>
2011-10-02 21:55:44 +0800

    added jquery mobile features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :prepare_for_mobile
+
+  private
+
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+
+  def prepare_for_mobile
+    request.format = :mobile if mobile_device?
+  end
 end
